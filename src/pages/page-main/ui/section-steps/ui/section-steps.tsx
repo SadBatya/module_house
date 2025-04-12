3bc2240dde93dbc778b6b02f551f70f2925d84c7
sadbatya <sadbatya@mail.ru>
2025-04-12 16:15:32 +0200

    feat: SectionSteps

--- src/pages/page-main/ui/section-steps/ui/section-steps.tsx
+++ src/pages/page-main/ui/section-steps/ui/section-steps.tsx
@@ -4,16 +4,25 @@ import style from './section.module.scss';
 
 import { listData } from '../model/data';
 import { ListItem } from '@/shared/ui/ListItem/ListItem';
+import insidePng from './assets/inside.png';
+import Image from 'next/image';
 
 export const SectionSteps = () => (
   <Section>
-    <div className={style.container_text}>
-      <Title>Этапы возведения вашего будущего дома</Title>
-      <ul className={style.list}>
-        {listData.map(({ title, subtext }, index) => (
-          <ListItem title={title} subtext={subtext} key={index} />
-        ))}
-      </ul>
+    <div className={style.inner}>
+      <div className={style.container_text}>
+        <Title>Этапы возведения вашего будущего дома</Title>
+        <ul>
+          {listData.map(({ title, subtext }, index) => (
+            <ListItem title={title} subtext={subtext} key={index} />
+          ))}
+        </ul>
+        <div className={style.grayBlock}>
+          Общий срок строительства занимает от 3 до 6 месяцев в зависимости от
+          площади, сложности проекта и уникальных особенностей
+        </div>
+      </div>
+      <Image className={style.img} src={insidePng} alt="inside" />
     </div>
   </Section>
 );
