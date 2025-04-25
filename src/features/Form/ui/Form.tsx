1777beb042a34e1f55351d98e2a92b072bd466b3
viachaslaumasilevich <mosilevich@gmail.com>
2025-04-25 09:51:13 -0700

    feat: ts lint

--- src/features/Form/ui/Form.tsx
+++ src/features/Form/ui/Form.tsx
@@ -1,8 +1,16 @@
 import { Input } from '@/shared/ui/Input/Input';
 import style from './Form.module.scss';
+import { Button } from '@/shared/ui';
 
 export const Form = () => (
   <form className={style.form}>
-    <Input placeholder={''} labelText={''} type={'submit'} />
+    <Input placeholder={'Имя'} labelText={'Ваше имя'} />
+    <Input placeholder={'Телефон'} labelText={'Телефон'} />
+    <Input placeholder={'Telegram'} labelText={'Ваш Telegram'} />
+    <Button className={style.btn}>Отправить заявку</Button>
+    <span className={style.subtext}>
+      Указывая свои данные вы даёте согласие на обработку персональных данных, в
+      соответствии с политикой конфиденциальности
+    </span>
   </form>
 );
