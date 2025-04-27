81ceabf15d840422644132fa6c2921ef101a1223
sadbatya <sadbatya@mail.ru>
2025-04-27 20:21:56 +0200

    fix: правки по свайперу

--- src/page/page-about/ui/section-license/ui/section-license.tsx
+++ src/page/page-about/ui/section-license/ui/section-license.tsx
@@ -1,22 +1,32 @@
 'use client';
+
 import { Section } from '@/shared/ui';
 import style from './section-license.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper as SwiperType } from 'swiper/types';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export const SectionLicense = () => {
   return (
     <Section className={style.section}>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={4}
+        spaceBetween={20}
+        slidesPerView={3.5}
+        loop={true}
+        className={style.swiper}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
+        <SwiperSlide className={style.swiper_slide}>Slide 1</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 2</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 3</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 4</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 5</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 6</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 7</SwiperSlide>
+        <SwiperSlide className={style.swiper_slide}>Slide 8</SwiperSlide>
       </Swiper>
     </Section>
   );
