443198127fd51c6b09e7a84b8d5142bc58394b4e
sadbatya <sadbatya@mail.ru>
2025-05-01 10:51:21 +0200

    style: адаптив section-what-we-building

--- src/page/page-main/ui/section-what-we-building/ui/section-what-we-building.tsx
+++ src/page/page-main/ui/section-what-we-building/ui/section-what-we-building.tsx
@@ -1,9 +1,10 @@
 import { Accordion } from '@/widgets/Accordion';
 import { Title } from '@/shared/ui';
 import { Section } from '@/shared/ui';
+import style from './section-what-we-building.module.scss';
 
 export const SectionWhatWeBuilding = () => (
-  <Section>
+  <Section className={style.section}>
     <Title>Что мы строим?</Title>
     <Accordion />
   </Section>
