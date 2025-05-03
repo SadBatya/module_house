72c9abac7fc2edc8666edaa89e1f81464db7cbad
viachaslaumasilevich <mosilevich@gmail.com>
2025-05-03 11:48:36 -0700

    test

--- src/page/page-about/ui/section-license/ui/section-license.tsx
+++ src/page/page-about/ui/section-license/ui/section-license.tsx
@@ -1,18 +1,21 @@
 'use client';
-import { Section } from '@/shared/ui';
+import { Section, Title } from '@/shared/ui';
 import style from './section-license.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const SectionLicense = () => {
   return (
     <Section className={style.section}>
+      <Title uppercase>
+        <span className={style.title_color}>«ЛиЦЕНЗИИ»</span>И ГОСТ
+      </Title>
       <Swiper
         spaceBetween={50}
-        slidesPerView={4}
+        slidesPerView={3.5}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide className={style.slide}>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
