66daa1d6b89f364b8b440e7d7245c759cd4ebc0b
sadbatya <sadbatya@mail.ru>
2025-05-03 12:04:19 +0200

    style: адаптив section-control

--- src/page/page-main/ui/section-control/ui/section-control.tsx
+++ src/page/page-main/ui/section-control/ui/section-control.tsx
@@ -1,14 +1,23 @@
 import { ImgWithText } from '@/widgets/ImgWithText/ImgWithText';
 import { Section } from '@/shared/ui';
 import phonePng from './assets/phone.png';
+import style from './section-control.module.scss';
 
 export const SectionControl = () => (
   <Section>
     <ImgWithText
+      titleUppercase={false}
+      className={style.inner}
       img={phonePng}
       title="Все под контролем"
-      description="Ежедневно вы будете получать фотоотчет о выполненной работе. 
-Для принятия работы вам не нужно разбираться в строительных деталях – каждому клиенту предоставляется подробный чек-лист, который поможет самостоятельно оценить качество выполненных работ."
+      description={
+        <>
+          Ежедневно вы будете получать фотоотчет о выполненной работе. <br />{' '}
+          <br /> Для принятия работы вам не нужно разбираться в строительных
+          деталях – каждому клиенту предоставляется подробный чек-лист, который
+          поможет самостоятельно оценить качество выполненных работ.
+        </>
+      }
     />
   </Section>
 );
--- src/widgets/ImgWithText/ImgWithText.tsx
+++ src/widgets/ImgWithText/ImgWithText.tsx
@@ -2,18 +2,27 @@ import { Title } from '@/shared/ui';
 import Image, { StaticImageData } from 'next/image';
 import style from './ImgWithText.module.scss';
 import { ReactNode } from 'react';
+import clsx from 'clsx';
 
 interface Props {
   title: string;
   description: string | ReactNode;
   img: StaticImageData;
+  className?: string;
+  titleUppercase?: boolean;
 }
 
-export const ImgWithText = ({ title, description, img }: Props) => (
-  <div className={style.inner}>
+export const ImgWithText = ({
+  title,
+  description,
+  img,
+  className,
+  titleUppercase = true,
+}: Props) => (
+  <div className={clsx(style.inner, className)}>
     <Image className={style.img} src={img} alt="men" />
     <div className={style.info}>
-      <Title uppercase>{title}</Title>
+      <Title uppercase={titleUppercase}>{title}</Title>
       <p className={style.description}>{description}</p>
     </div>
   </div>
