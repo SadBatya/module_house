d4d37cf2771b092b2eed68432af7c704468eff1a
sadbatya <sadbatya@mail.ru>
2025-05-04 14:00:37 +0200

    refactor: section-structured-clone анимация и копирайт

--- src/page/page-main/ui/section-guarantee/model/data.ts
+++ src/page/page-main/ui/section-guarantee/model/data.ts
@@ -12,14 +12,14 @@ interface ICardInfo {
 export const cardInfo: ICardInfo[] = [
   {
     icon: icon_1,
-    description: 'Цена на строительство не меняется',
+    description: 'Фиксируем все требования в договоре',
   },
   {
     icon: icon_2,
-    description: 'Гарантийное обслуживание 5 лет',
+    description: 'Даем 10 лет гарантию на каркас и 15 лет из камня',
   },
   {
     icon: icon_3,
-    description: 'Сдаем дом точно в срок',
+    description: '0,1% штрафа за каждый день просрочки!',
   },
 ];
