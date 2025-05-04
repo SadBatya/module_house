0bc54dad7be99863ee109fbd1091c3e78d53ae6f
sadbatya <sadbatya@mail.ru>
2025-05-04 12:59:04 +0200

    chore: убрал конфиг для работы с svg

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,6 @@ const nextConfig: NextConfig = {
     additionalData: `@use "variables" as *; @use "mixins" as *;`,
     includePaths: [path.join(__dirname, 'src/styles')],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
