57fdda2abab382539560b72dc1a820b7a22018a4
viachaslaumasilevich <mosilevich@gmail.com>
2025-05-10 09:01:38 -0700

    refactor: about page bug fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,15 @@
+import { SectionCompany } from '@/page/page-about/ui/section-company';
+import { SectionProduction } from '@/page/page-about/ui/section-production';
+import { SectionLicense } from '@/page/page-about/ui/section-license';
+import { SectionMotivation } from '@/page/page-about/ui/section-motivation';
+
 export default function About() {
-  return <div>adsasdsad</div>;
+  return (
+    <>
+      <SectionCompany />
+      <SectionProduction />
+      <SectionLicense />
+      <SectionMotivation />
+    </>
+  );
 }
