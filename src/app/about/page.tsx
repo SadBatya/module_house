75713f8f31d8dd57d9aac15a77bbe40ccad6fffb
viachaslaumasilevich <mosilevich@gmail.com>
2025-05-12 11:58:56 -0700

    refactor: breadcrumbs added to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,10 +3,20 @@ import { SectionProduction } from '@/page/page-about/ui/section-production';
 import { SectionLicense } from '@/page/page-about/ui/section-license';
 import { SectionMotivation } from '@/page/page-about/ui/section-motivation';
 import { SectionReviews } from '@/page/page-about/ui/section-reviews';
+import { INTERNAL_ROUTES } from '@/shared/routes';
+import { Breadcrumbs } from '@/shared/ui';
 
 export default function About() {
   return (
     <>
+      <Breadcrumbs
+        items={[
+          {
+            label: 'О нас',
+            path: INTERNAL_ROUTES.ABOUT,
+          },
+        ]}
+      />
       <SectionCompany />
       <SectionProduction />
       <SectionLicense />
