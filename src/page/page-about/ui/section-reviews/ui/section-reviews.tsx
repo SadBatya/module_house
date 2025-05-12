c532b197f109661711a8638258631d2c616cfbc9
viachaslaumasilevich <mosilevich@gmail.com>
2025-05-12 16:56:37 -0700

    refactor: section reviews added

--- src/page/page-about/ui/section-reviews/ui/section-reviews.tsx
+++ src/page/page-about/ui/section-reviews/ui/section-reviews.tsx
@@ -1,18 +1,95 @@
+'use client';
 import style from './section-reviews.module.scss';
 import { Section } from '@/shared/ui';
+import Image from 'next/image';
+import star from './assets/star.svg';
+import arrow from './assets/arrow.svg';
+import { reviews } from '../model/data';
+import { useState } from 'react';
 
-export const SectionReviews = () => (
-  <Section>
-    <div className={style.top_part}>
-      <p className={style.text_left_part}>Отзывы наших клиентов</p>
-      <span className={style.text_right_part}>
-        Реальные отзывы о нашей компании вы можете посмотреть на независимых
-        сайтах
-      </span>
-      <button className={style.button}>
-        <span className={style.text_1}>Подобрать проект для себя </span>
-        <span className={style.text_2}>(5 вопросов)</span>
-      </button>
-    </div>
-  </Section>
-);
+export const SectionReviews = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentReview = reviews[currentIndex];
+
+  const nextReview = () => {
+    if (currentIndex !== reviews.length - 1) {
+      setCurrentIndex((prev) => prev + 1);
+    }
+  };
+
+  const previousReview = () => {
+    if (currentIndex !== 0) {
+      setCurrentIndex((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <Section>
+      <div className={style.top_part}>
+        <p className={style.text_left_part}>Отзывы наших клиентов</p>
+        <span className={style.text_right_part}>
+          Реальные отзывы о нашей компании вы можете посмотреть на независимых
+          сайтах
+        </span>
+        <button className={style.button}>
+          <span className={style.text_1}>Подобрать проект для себя </span>
+        </button>
+      </div>
+      <div className={style.main_card_section}>
+        <div className={style.card_section}>
+          <div className={style.info_text}>
+            {[...Array(currentReview.rating)].map((el, index) => (
+              <Image
+                key={index}
+                src={star}
+                alt={'Rate'}
+                className={style.star}
+              />
+            ))}
+            <div className={style.name_date}>
+              <span className={style.name}>{currentReview.username}</span>
+              <span className={style.date}>{currentReview.date}</span>
+            </div>
+            <p className={style.text_review}>{currentReview.review}</p>
+            <button className={style.button_text}>Читать далее ..... </button>
+            <Image
+              className={style.avatar}
+              alt={'Avatarka'}
+              src={currentReview.avatar}
+            />
+          </div>
+          <div className={style.info_photo}>
+            <Image
+              className={style.photo}
+              src={currentReview.photo}
+              alt={'Photo reviews'}
+            />
+          </div>
+          <div className={style.bottom}>
+            <div className={style.counter}>
+              <span className={style.number}>{currentIndex + 1}</span>
+              <span className={style.line}>|</span>
+              <span className={style.all}>{reviews.length}</span>
+            </div>
+            <div className={style.arrows}>
+              <Image
+                onClick={currentIndex !== 0 ? previousReview : undefined}
+                src={arrow}
+                alt={'arrow'}
+                className={style.arrow_left}
+              />
+              <Image
+                onClick={
+                  currentIndex !== reviews.length - 1 ? nextReview : undefined
+                }
+                src={arrow}
+                alt={'arrow'}
+                className={style.arrow_right}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
