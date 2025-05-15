c68d3c5751ec11ad5b21a8b12d02f3eea7d326e5
sadbatya <sadbatya@mail.ru>
2025-05-15 08:49:21 +0200

    feat: добавил комментарии по коду для правок

--- src/page/page-about/ui/section-reviews/ui/section-reviews.tsx
+++ src/page/page-about/ui/section-reviews/ui/section-reviews.tsx
@@ -9,15 +9,18 @@ import { useState } from 'react';
 
 export const SectionReviews = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  // TODO Эту переменную можно убрать и сразу использовать в map и туда сразу прокидывать нужный индекс
   const currentReview = reviews[currentIndex];
 
   const nextReview = () => {
+    // TODO здесь сделать логику, если это последний слайдер то при клике скидывать на первый чтобы был бесконечный цикл
     if (currentIndex !== reviews.length - 1) {
       setCurrentIndex((prev) => prev + 1);
     }
   };
 
   const previousReview = () => {
+    // TODO тут тоже самое
     if (currentIndex !== 0) {
       setCurrentIndex((prev) => prev - 1);
     }
@@ -26,12 +29,15 @@ export const SectionReviews = () => {
   return (
     <Section>
       <div className={style.top_part}>
+        {/* TODO тут надо использовать компонент Title и добавить класс и поправить стили если надо */}
         <p className={style.text_left_part}>Отзывы наших клиентов</p>
+        {/* TODO тут изменить на компонент P и также добавить класс и изменить стили */}
         <span className={style.text_right_part}>
           Реальные отзывы о нашей компании вы можете посмотреть на независимых
           сайтах
         </span>
         <button className={style.button}>
+          {/* TODO span можно убрать и назначить стили через класс button */}
           <span className={style.text_1}>Подобрать проект для себя </span>
         </button>
       </div>
@@ -54,7 +60,6 @@ export const SectionReviews = () => {
               src={currentReview.avatar}
             />
           </div>
-
           <div className={style.info_text}>
             <div className={style.name_date}>
               <span className={style.name}>{currentReview.username}</span>
@@ -63,6 +68,7 @@ export const SectionReviews = () => {
             <p className={style.text_review}>{currentReview.review}</p>
           </div>
           <button className={style.button_text}>Читать далее .....</button>
+          {/* TODO слишком много точек достаточно поставить ... */}
           <div className={style.info_photo}>
             <Image
               className={style.photo}
@@ -75,6 +81,8 @@ export const SectionReviews = () => {
       <div className={style.bottom}>
         <div className={style.counter}>
           <span className={style.number}>{currentIndex + 1}</span>
+          {/* TODO тут можно сделать буквой разделитель, но думаю лучше будет сделать бордером или bg-color и шириной блока и высотой */}
+          {/* В данной ситуации ок */}
           <span className={style.line}>|</span>
           <span className={style.all}>{reviews.length}</span>
         </div>
